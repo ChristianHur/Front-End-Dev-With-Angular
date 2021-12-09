2b23fb31551431211ea4780b318da96697a71d77
Christian Hur <chrisvhur@gmail.com>
2021-12-09 12:09:27 -0600

    Update flight.service.ts

--- angular8/flightscheduler/src/app/services/flight.service.ts
+++ angular8/flightscheduler/src/app/services/flight.service.ts
@@ -2,44 +2,44 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable, Observer } from "rxjs";
 import { Flight } from "../models/flight";
+import { api } from '../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlightService {
 
-  private endpoint = 'http://127.0.0.1:8000/flights/';
-
   constructor(private http: HttpClient) { }
 
   //GET A SINGLE FLIGHT
   getFlight(id: number): Observable<any>{
-    return this.http.get(this.endpoint + id);
+    return this.http.get(api + id);
   }
 
   //GET All Flights
   getAllFlights(): Observable<any>{
-    return this.http.get(this.endpoint)
+    return this.http.get(api)
   }
 
   //POST - Add a new flight
   flightCreate(flight: Flight): Observable<object>{
-    return this.http.post(this.endpoint,flight);
+    return this.http.post(api,flight);
   }
 
   //PUT - Update
   updateFlight(id: number, payload: any): Observable<object>{
-    return this.http.put(this.endpoint + id, payload);
+    return this.http.put(api + id, payload);
   }
 
   //DELETE A FLIGHT
   deleteFlight(id: number): Observable<any>{
-    return this.http.delete(this.endpoint + id);
+    return this.http.delete(api + id);
   }
 
   //DELETE ALL FLIGHTS
   deleteAllFlights(): Observable<any>{
-    return this.http.delete(this.endpoint);
+    return this.http.delete(api);
   }
 
 }
